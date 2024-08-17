49971d1771a63c778865455c2a3a1bacabe68917
Biruk Ayalew <138154129+Biruk7479@users.noreply.github.com>
2024-08-17 05:06:37 +0300

    Time: 42 ms (31.45%) | Memory: 16.5 MB (42.98%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,9 +1,10 @@
+import copy
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        mylist = [row[:] for row in matrix]
+        mylist = copy.deepcopy(matrix)
         n=len(matrix)
         for i in range(n):
             for j in range(n):
