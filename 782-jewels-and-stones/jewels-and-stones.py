5984fa3d0f16bb1bdb34f49d5d052a84d04b5a63
Biruk Ayalew <138154129+Biruk7479@users.noreply.github.com>
2024-08-18 20:10:46 +0300

    Time: 35 ms (59.43%) | Memory: 16.4 MB (65.66%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -3,11 +3,9 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
       counter = Counter(stones)
       num=0
-      inx=0
       for  i in jewels:
         if i in counter:
             num+=counter[i]
-            inx+=1
       return num      
 
 
