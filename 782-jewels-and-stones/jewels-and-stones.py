a9a092ac4758d837c07a2845f4abdce7481c3937
Biruk Ayalew <138154129+Biruk7479@users.noreply.github.com>
2024-08-18 20:17:08 +0300

    Time: 39 ms (29.91%) | Memory: 16.6 MB (23.52%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -2,10 +2,10 @@ from collections import Counter
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
       counter = Counter(stones)
-      s=set(jewels)
+      s=set(stones)
       num=0
-      for  i in s:
-        if i in counter:
+      for  i in jewels:
+        if i in s:
             num+=counter[i]
       return num      
 
