94786765e1e352f61d25b2a607b0353af3d135e1
Biruk Ayalew <138154129+Biruk7479@users.noreply.github.com>
2024-08-19 11:27:53 +0300

    Time: 169 ms (62.89%) | Memory: 18 MB (19.52%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -6,3 +6,19 @@ class Solution:
         for num in h:
              if h[num] > len(nums) // 2:
                 return num
+
+                
+                # majority = None
+                #         count = 0
+
+                #         for num in nums:
+                #             if count == 0:
+                #                 majority = num
+                #                 count += 1
+                #             else:
+                #                 if num != majority:
+                #                     count -= 1
+                #                 else:
+                #                     count += 1
+                        
+                #         return majority                
