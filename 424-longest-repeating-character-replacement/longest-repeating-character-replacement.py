beb303fe925592215e9c085d90beb001035f20c7
Biruk Ayalew <138154129+Biruk7479@users.noreply.github.com>
2025-04-08 19:39:13 +0300

    Time: 221 ms (18.35%) | Memory: 18 MB (49.13%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,14 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         longest=0
+        count=[0]*26
         l=0
-        counts= [0]*26
-
+#starting from the beginning count the number of the abundant characters in the string
         for r in range(len(s)):
-            counts[ord(s[r])-65]+=1
+            count[ord(s[r])-65]+=1
 
-            while (r-l +1) - max(counts)>k:
-                counts[ord(s[l]) - 65] -= 1
+            while (r-l+1) - max(count) > k:
+                count[ord(s[l])-65]-=1
                 l+=1
-            longest = max(longest, r-l+1)
+            longest = max(longest ,(r-l+1))
+
         return longest
